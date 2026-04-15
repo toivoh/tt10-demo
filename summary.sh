7276720cbde1503ac404a66bf8c04bc1082e5071
Toivo Henningsson <toivo.h.h@gmail.com>
2026-04-15 18:24:56 +0200

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -13,11 +13,11 @@ echo "Cell usage"
 echo "=========="
 grep "Number of" runs/wokwi/06-yosys-synthesis/yosys-synthesis.log | tail -n 8
 #grep "dfxtp_.  " runs/wokwi/06-yosys-synthesis/yosys-synthesis.log
-./tt/tt_tool.py --openlane2 --print-cell-summary | grep dfxtp
-#./tt/tt_tool.py --openlane2 --print-cell-summary | grep dlxtp
-#./tt/tt_tool.py --openlane2 --print-cell-summary | grep dlygate
-#./tt/tt_tool.py --openlane2 --print-cell-summary | grep Buffer
-#./tt/tt_tool.py --openlane2 --print-cell-summary | grep Total
+./tt/tt_tool.py --print-cell-summary | grep dfxtp
+#./tt/tt_tool.py --print-cell-summary | grep dlxtp
+#./tt/tt_tool.py --print-cell-summary | grep dlygate
+#./tt/tt_tool.py --print-cell-summary | grep Buffer
+#./tt/tt_tool.py --print-cell-summary | grep Total
 grep "Chip area" runs/wokwi/06-yosys-synthesis/yosys-synthesis.log
 echo
 
@@ -28,12 +28,12 @@ echo
 
 echo "STA"
 echo "==="
-grep "timing__setup__ws\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
-grep "timing__hold__ws\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
+grep "timing__setup__ws\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
+grep "timing__hold__ws\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
 echo
-grep "design__max_slew_violation__count\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
-grep "design__max_fanout_violation__count\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
-grep "design__max_cap_violation__count\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
+grep "design__max_slew_violation__count\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
+grep "design__max_fanout_violation__count\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
+grep "design__max_cap_violation__count\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
 echo
 
 
@@ -42,10 +42,10 @@ echo "======="
 echo "# "
 echo
 grep Util runs/wokwi/*-openroad-globalplacement/openroad-globalplacement.log
-grep util runs/wokwi/68-misc-reportmanufacturability/state_out.json
+grep util runs/wokwi/*-misc-reportmanufacturability/state_out.json
 echo
-grep "timing__setup__ws\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
-grep "timing__hold__ws\"" runs/wokwi/68-misc-reportmanufacturability/state_out.json
+grep "timing__setup__ws\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
+grep "timing__hold__ws\"" runs/wokwi/*-misc-reportmanufacturability/state_out.json
 echo "Max slew violation: "
 echo
-#./tt/tt_tool.py --openlane2 --print-cell-summary
+#./tt/tt_tool.py --print-cell-summary
